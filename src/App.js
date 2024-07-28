bc07d8ac03574c9edaa8d9f0620ff007c60c6e5e
mozyanari <masanari7430@gmail.com>
2024-07-29 00:53:26 +0900

    fix: :rotating_light: EsLintに注意に対応

--- src/App.js
+++ src/App.js
@@ -1,39 +1,46 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import ROSLIB from "roslib";
 
 function App() {
   const [connectionStatus, setConnectionStatus] = useState("Disconnected");
-  const [cmdVel, setCmdVel] = useState("");
+  // const [cmdVel, setCmdVel] = useState("");
   const [x, setX] = useState("none");
 
-  let roslibConnector;
-  let topicCmdVel;
+  const roslibConnector = useRef(
+    new ROSLIB.Ros({
+      url: "ws://localhost:9090",
+    })
+  );
+  const topicCmdVel = useRef(
+    new ROSLIB.Topic({
+      ros: roslibConnector.current,
+      name: "/turtle1/cmd_vel",
+      messageType: "geometry_msgs/msg/Twist",
+    })
+  );
 
   useEffect(() => {
-    roslibConnector = new ROSLIB.Ros({
-      url: "ws://localhost:9090",
-    });
-    roslibConnector.on("connection", function () {
+    roslibConnector.current.on("connection", function () {
       console.log("Connected to ROSBridge WebSocket server.");
       setConnectionStatus("Connected");
     });
 
-    roslibConnector.on("error", function (error) {
+    roslibConnector.current.on("error", function (error) {
       console.log("Error connecting to ROSBridge WebSocket server: ", error);
       setConnectionStatus("Error");
     });
 
-    roslibConnector.on("close", function () {
+    roslibConnector.current.on("close", function () {
       console.log("Connection to ROSBridge WebSocket server closed.");
       setConnectionStatus("Disconnected");
     });
 
-    topicCmdVel = new ROSLIB.Topic({
-      ros: roslibConnector,
-      name: "/turtle1/cmd_vel",
-      messageType: "geometry_msgs/msg/Twist",
-    });
+    // topicCmdVel = new ROSLIB.Topic({
+    //   ros: roslibConnector,
+    //   name: "/turtle1/cmd_vel",
+    //   messageType: "geometry_msgs/msg/Twist",
+    // });
   }, []);
 
   // const topicPose = new ROSLIB.Topic({
@@ -43,7 +50,7 @@ function App() {
   // });
 
   useEffect(() => {
-    topicCmdVel.subscribe((message) => {
+    topicCmdVel.current.subscribe((message) => {
       console.log(message.angular);
       console.log(message.angular);
       setX(message.linear.x);
