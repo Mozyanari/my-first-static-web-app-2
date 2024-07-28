3261752b13aecad3a624d316c60e0bff14297418
mozyanari <masanari7430@gmail.com>
2024-07-28 22:10:36 +0900

    feat: :sparkles: roslibの接続するためのクライアントを追加

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React from "react";
+import { useEffect, useState } from "react";
+import ROSLIB from "roslib";
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [connectionStatus, setConnectionStatus] = useState("Disconnected");
+  useEffect(() => {
+    // localhostに接続
+    const roslibConnector = new ROSLIB.Ros({
+      url: "ws://localhost:9090",
+    });
+
+    roslibConnector.on("connection", function () {
+      console.log("Connected to ROSBridge WebSocket server.");
+      setConnectionStatus("Connected");
+    });
+
+    roslibConnector.on("error", function (error) {
+      console.log("Error connecting to ROSBridge WebSocket server: ", error);
+      setConnectionStatus("Error");
+    });
+
+    roslibConnector.on("close", function () {
+      console.log("Connection to ROSBridge WebSocket server closed.");
+      setConnectionStatus("Disconnected");
+    });
+  }, []);
+
+  return (
+    <>
+      <p>ROS Connection Status: {connectionStatus}</p>
+    </>
+  );
 }
 
 export default App;
